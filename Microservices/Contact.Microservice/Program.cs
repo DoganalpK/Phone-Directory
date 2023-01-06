c979571e0514fcb30fe8801e5c2d99b59e59d535
Doganalp KARATAS <doganalp.karatas@avatek.com.tr>
2023-01-06 17:01:23 +0300

    #00007: add dependencies to programcs

--- Microservices/Contact.Microservice/Program.cs
+++ Microservices/Contact.Microservice/Program.cs
@@ -1,3 +1,5 @@
+using Contact.Microservice;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+#region ServiceRegistration
+
+builder.Services.RegisterDependencies(builder.Configuration);
+
+#endregion
 
 var app = builder.Build();
 
---
+++ Microservices/Contact.Microservice/ServiceRegistration.cs
@@ -0,0 +1,16 @@
+﻿using Contact.Microservice.Models.DbContexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Contact.Microservice
+{
+    public static class ServiceRegistration
+    {
+        public static void RegisterDependencies(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(opt =>
+            {
+                opt.UseNpgsql(configuration.GetConnectionString("PostgresDb"));
+            });
+        }
+    }
+}
