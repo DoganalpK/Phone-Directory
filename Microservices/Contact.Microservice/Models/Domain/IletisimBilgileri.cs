5b1c37d5990a1a9f83e6c53c12b0858d06b383ed
Doganalp KARATAS <doganalp.karatas@avatek.com.tr>
2023-01-06 13:50:18 +0300

    00002:created domains

---
+++ Microservices/Contact.Microservice/Models/Domain/IletisimBilgileri.cs
@@ -0,0 +1,14 @@
+﻿using Contact.Microservice.Models.Base;
+
+namespace Contact.Microservice.Models.Domain
+{
+    public class IletisimBilgileri : BaseEntity
+    {
+        public string? BilgiIcerigi { get; set; }
+        public string? TelefonNumarasi { get; set; }
+        public string? EmailAdresi { get; set; }
+        public string? Konum { get; set; }
+        public Guid KisilerUUID { get; set; }
+        public Kisiler Kisiler { get; set; }
+    }
+}
---
+++ Microservices/Contact.Microservice/Models/Domain/Kisiler.cs
@@ -0,0 +1,12 @@
+﻿using Contact.Microservice.Models.Base;
+
+namespace Contact.Microservice.Models.Domain
+{
+    public class Kisiler: BaseEntity
+    {
+        public string? Ad { get; set; }
+        public string? Soyad { get; set; }
+        public string? Firma { get; set; }
+        public List<IletisimBilgileri>? IletisimBilgisi { get; set; }
+    }
+}
