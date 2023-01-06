eb909605bd2ed8de32f4c7b41d6249be4296f4f4
Doganalp KARATAS <doganalp.karatas@avatek.com.tr>
2023-01-06 13:58:35 +0300

    00003:created dbcontext

---
+++ Microservices/Contact.Microservice/Models/DbContexts/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Contact.Microservice.Models.DbContexts
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Domain.Kisiler> Kisiler { get; set; }
+        public DbSet<Domain.IletisimBilgileri> IletisimBilgileri { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
