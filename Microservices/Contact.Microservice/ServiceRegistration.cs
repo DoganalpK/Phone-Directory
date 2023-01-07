7c62a933def9cc92ad9822e035d9cde31e1eb031
Doganalp KARATAS <doganalp.karatas@avatek.com.tr>
2023-01-07 11:12:45 +0300

    #00016 add service registraion for automapper and mediator

--- Microservices/Contact.Microservice/ServiceRegistration.cs
+++ Microservices/Contact.Microservice/ServiceRegistration.cs
@@ -1,6 +1,8 @@
 ﻿using Contact.Microservice.Models.DbContexts;
 using Contact.Microservice.Repository;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 namespace Contact.Microservice
 {
@@ -8,12 +10,16 @@ namespace Contact.Microservice
     {
         public static void RegisterDependencies(this IServiceCollection services, IConfiguration configuration)
         {
+            var assmb = Assembly.GetExecutingAssembly();
+
             services.AddDbContext<AppDbContext>(opt =>
             {
                 opt.UseNpgsql(configuration.GetConnectionString("PostgresDb"));
             });
 
             services.AddScoped(typeof(IGenericRepository<>),typeof(GenericRepository<>));
+            services.AddAutoMapper(assmb);
+            services.AddMediatR(assmb);
         }
     }
 }
