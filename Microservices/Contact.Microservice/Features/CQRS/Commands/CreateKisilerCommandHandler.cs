094fccc71286befcb1e5715dafbb6d22a057d3fc
Doganalp KARATAS <doganalp.karatas@avatek.com.tr>
2023-01-07 11:38:13 +0300

    #00019 kisiler request and handler

--- Microservices/Contact.Microservice/Features/CQRS/Commands/CreateKisilerCommandHandler.cs
+++ Microservices/Contact.Microservice/Features/CQRS/Commands/CreateKisilerCommandHandler.cs
@@ -1,6 +1,27 @@
-﻿namespace Contact.Microservice.Features.CQRS.Commands
+﻿using AutoMapper;
+using Contact.Microservice.Models.Domain;
+using Contact.Microservice.Models.Dtos;
+using Contact.Microservice.Repository;
+using MediatR;
+
+namespace Contact.Microservice.Features.CQRS.Commands
 {
-    public class CreateKisilerCommandHandler
+    public class CreateKisilerCommandHandler : IRequestHandler<CreateKisilerCommandRequest, KisilerListDto>
     {
+        private readonly IGenericRepository<Kisiler> _repository;
+        private readonly IMapper _mapper;
+
+        public CreateKisilerCommandHandler(IGenericRepository<Kisiler> repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<KisilerListDto> Handle(CreateKisilerCommandRequest request, CancellationToken cancellationToken)
+        {
+            var dto = _mapper.Map<Kisiler>(request);
+            var kisi = await _repository.CreateAsync(dto);
+            return _mapper.Map<KisilerListDto>(kisi);
+        }
     }
 }
--- Microservices/Contact.Microservice/Features/CQRS/Commands/CreateKisilerCommandRequest.cs
+++ Microservices/Contact.Microservice/Features/CQRS/Commands/CreateKisilerCommandRequest.cs
@@ -1,6 +1,12 @@
-﻿namespace Contact.Microservice.Features.CQRS.Commands
+﻿using Contact.Microservice.Models.Dtos;
+using MediatR;
+
+namespace Contact.Microservice.Features.CQRS.Commands
 {
-    public class CreateKisilerCommandRequest
+    public class CreateKisilerCommandRequest : IRequest<KisilerListDto>
     {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string Firma { get; set; }
     }
 }
