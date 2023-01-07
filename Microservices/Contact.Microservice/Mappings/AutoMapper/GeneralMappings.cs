fca5a8edd9a2c517940270f94fb79ebd7b7c308c
Doganalp KARATAS <doganalp.karatas@avatek.com.tr>
2023-01-07 11:10:54 +0300

    #00015 create general mappings

---
+++ Microservices/Contact.Microservice/Mappings/AutoMapper/GeneralMappings.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Contact.Microservice.Models.Domain;
+using Contact.Microservice.Models.Dtos;
+
+namespace Contact.Microservice.Mappings.AutoMapper
+{
+    public class GeneralMappings : Profile
+    {
+        public GeneralMappings()
+        {
+            CreateMap<Kisiler, KisilerCreateDto>().ReverseMap();
+            CreateMap<Kisiler, KisilerListDto>().ReverseMap();
+            CreateMap<IletisimBilgileri, KisilerIletisimBilgileriCreateDto>().ReverseMap();
+            CreateMap<IletisimBilgileri, KisilerIletisimBilgileriListDto>().ReverseMap();
+        }
+    }
+}
