c0c51d8725d23da6c26cf40eff8045b5642817d1
Doganalp KARATAS <doganalp.karatas@avatek.com.tr>
2023-01-08 20:56:57 +0300

    .

--- Microservices/Report.Microservice/Consumers/KisiRaporConsumer.cs
+++ Microservices/Report.Microservice/Consumers/KisiRaporConsumer.cs
@@ -1,4 +1,5 @@
 ﻿using MassTransit;
+using Newtonsoft.Json;
 using Shared.Models.Models;
 using System.Text;
 using System.Text.Json;
@@ -17,8 +18,10 @@ namespace Report.Microservice.Consumers
         public async Task Consume(ConsumeContext<Rapor> context)
         {
             var obj = context.Message;
-            //var client = _clientFactory.CreateClient();
-            //var response = await client.GetAsync("https://localhost:7088/api/Kisi");
+            var client = _clientFactory.CreateClient();
+            var httpContent = new StringContent(JsonConvert.SerializeObject(obj)
+                    , Encoding.UTF8, "application/json");
+            await client.PostAsync("https://localhost:7088/api/Rapor", httpContent);
             //var jsonData = await response.Content.ReadAsStringAsync();
         }
     }
