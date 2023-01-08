4502193f68e0a88f82e8e16276cf5d44f29b0f62
Doganalp KARATAS <doganalp.karatas@avatek.com.tr>
2023-01-08 15:29:16 +0300

    #00033 create shared model and referenced

---
+++ Shared/Shared.Models/Models/Report.cs
@@ -0,0 +1,17 @@
+﻿namespace Shared.Models.Models
+{
+    public class Report
+    {
+        public Guid UUID { get; set; }
+        public string Konum { get; set; }
+        public int KisiSayisi { get; set; }
+        public int TelefonNumarasiSayisi { get; set; }
+        public DateTime RaporTalepTarihi { get; set; }
+    }
+
+    public enum ReportStatus
+    {
+        Hazirlaniyor = 0,
+        Tamamlandi = 1,
+    }
+}
